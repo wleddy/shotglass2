f8c68f03ed878fa2b83a00b0a1b386f6a447a694
Bill Leddy <bill@williesworkshop.net>
2020-11-18 16:27:27 -0800

    add method to add tools menu

--- tools/views/tools.py
+++ tools/views/tools.py
@@ -1,18 +1,27 @@
 from flask import request, session, g, redirect, url_for, \
-     render_template, flash, Blueprint
-from shotglass2.takeabeltof.utils import printException, cleanRecordID
+     render_template, flash, Blueprint, Response
 from shotglass2.users.admin import login_required, table_access_required
+from shotglass2.users.models import User
 from shotglass2.shotglass import ShotLog
 
 mod = Blueprint('tools',__name__, template_folder='templates/', url_prefix='/tools')
 
-
-def setExits():
-    g.listURL = url_for('.display')
-    g.editURL = url_for('.edit')
-    g.deleteURL = url_for('.display') + 'delete/'
-    g.title = 'Tools'
-
+def register_admin():
+    if not g.admin:
+        g.admin = Admin(g.db) # This is where user access rules are stored
+    g.admin.register(User,
+            '',
+            display_name='Tools',
+            minimum_rank_required=500,
+            header_row=True
+        )
+    g.admin.register(User,
+            url_for('tools.view_log'),
+            display_name='View Log',
+            minimum_rank_required=500,
+        )
+        
+    
 @mod.route('/view_log',methods=['GET',])
 @mod.route('/view_log/',methods=['GET',])
 @login_required
@@ -23,3 +32,4 @@ def view_log():
     
     return render_template('log_viewer.html',log=log)
     
+
