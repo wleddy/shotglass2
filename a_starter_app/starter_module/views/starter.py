546db8e08983b984b5635a485c7198f916dacdaa
Bill Leddy <bill@williesworkshop.net>
2024-01-30 16:06:16 -0800

    Update the sample code

--- a_starter_app/starter_module/views/starter.py
+++ a_starter_app/starter_module/views/starter.py
@@ -5,7 +5,7 @@ from shotglass2.users.admin import login_required, table_access_required
 from shotglass2.takeabeltof.views import TableView, EditView
 from shotglass2.takeabeltof.jinja_filters import plural
 
-import travel_log.models as models
+import starter_module.models as models
 
 PRIMARY_TABLE = models.SampleTable
 MOD_NAME = PRIMARY_TABLE.TABLE_IDENTITY
@@ -18,7 +18,6 @@ def setExits():
     g.editURL = url_for('.edit')
     g.deleteURL = url_for('.display') + 'delete/'
     g.title = f'{plural(PRIMARY_TABLE(g.db).display_name,2)}'
-
 
 
 # this handles table list and record delete
@@ -46,33 +45,64 @@ def display(path=None):
 def edit(rec_id=None):
     setExits()
     g.title = "Edit {} Record".format(g.title)
+ 
     view = EditView(PRIMARY_TABLE,g.db,rec_id)
+    # Optonally may want to specify the edit fields to use with default edit form
+    # otherwise all fields will be included except forgien keys
 
+    # view.edit_fields = []
+    
+    # view.edit_fields.extend(
+    #     [
+    #     {'name':'location_name','req':True,},
+    #     {'name':'entry_type','req':True,'type':'select','options':[
+    #         {'name':'Departure'},
+    #         {'name':'Point of Interest'},
+    #         {'name':'Arrival'},
+    #     ]},
+    #     {'name':'entry_date','req':True,'type':'datetime','label':'When'},
+    #     ]
+    # )
+    # entry_date_dict = {'name':'entry_date','type':'raw','content':''}
+    # entry_date_dict['content'] = """<p><strong>Some Raw HTML</strong></p>"""
+    # view.edit_fields.extend([entry_date_dict])
+
+    # Some methods in view you can override
+    # view.validate_form = validate_form # view does almost no validation
+    # view.after_get_hook = ? # view has just loaded the record from disk
+    # view.before_commit_hook = ? # view is about to commit the record
+
+    # if is_mobile_device():
+    #     # Sometimes not as convenient on mobile...
+    #     view.use_anytime_date_picker = False
+
+
+    # Process the form?
     if request.form:
-        table = PRIMARY_TABLE(g.db)
-        id = cleanRecordID(request.form.get('id',-1))
-        if id < 0:
-            return redirect(g.listURL)
-        if id == 0:
-            rec = table.new()
-        else:
-            rec = table.get(id)
-        if not rec:
-            flash(f'{table.display_name} record not found')
-        else:
-            table.update(rec,request.form)
-            if validForm(rec):
-                table.save(rec)
+        view.update(save_after_update=True)
+        if view.success:
             return redirect(g.listURL)
 
+    # otherwise send the list...
     return view.render()
 
     
-def validForm(rec):
+def validate_form(view):
     # Validate the form
-    goodForm = True
-                
-    return goodForm
+    valid_form = True
+    view._set_edit_fields()
+    for field in view.edit_fields:
+        if field['name'] in request.form and field['req']:
+            val = view.rec.__getattribute__(field['name'])
+            if isinstance(val,str):
+                val = val.strip()
+            if not val:
+                view.result_text = "You must enter a value for {}".format(field['name'])
+                flash(view.result_text)
+                view.success = False
+                valid_form = False
+            
+    return valid_form
 
     
 def create_menus():
@@ -106,7 +136,7 @@ def create_menus():
 
 def register_blueprints(app, subdomain = None) -> None:
     """
-    Register this module with the app for this module
+    Register one or more modules with the Flask app
 
     Arguments:
         app -- the current app
